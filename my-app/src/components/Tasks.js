daf20d4abbd186743b52cd2a5f4c7574c544dca8
Valerii Bihun <valeriibihun.co@gmail.com>
2021-12-23 23:18:14 -0700

    Update Tasks.js

---
+++ my-app/src/components/Tasks.js
@@ -0,0 +1,32 @@
+const tasks = [
+    {
+        id:1,
+        text: 'Doctors App',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Doctors App',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Doctors App',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+]
+
+
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
